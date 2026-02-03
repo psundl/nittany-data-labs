798a42bf3d10e7fc83b84dba2a2fb6f1f3bcd5dd
Hellothereobi123 <tharun.dilliraj@gmail.com>
2026-02-03 14:30:57 -0500

    carousel addition

---
+++ ndl_website/src/Carousel.jsx
@@ -0,0 +1,22 @@
+import logo from './assets/ndl_logo.png';
+import ImageGallery from "react-image-gallery"
+import './Base.css'
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
+function MyGallery() {
+  return <ImageGallery items={images} />;
+}
+export default MyGallery;
